9ac283216282224bdbe045e193247cb9307afb6c
Mateusz Zarudzki <zarudzki@9livesdata.com>
2019-03-12 11:33:01 +0100

    impove a way processes are communicating between each other

--- main_child.cpp
+++ main_child.cpp
@@ -7,10 +7,14 @@
 
 int main(int argc, char *argv[])
 {
-  const int READ = 0;
-  const int WRITE = 1;
-  int child_to_parent[2];
-//   int file_descriptor;// = std::stoi(argv[1]);
+  printf("Child process starts\n");
+  printf("Child arguments are:\n");
+  for (int i = 0; i < argc; ++i){
+    printf("%d : %s\n", i, argv[i]);
+  }  
+
+  int child_to_parent;
+  int file_descriptor;
 //   // child_to_parent[0] = std::stoi(argv[2]);
 //   // child_to_parent[1] = std::stoi(argv[3]);
 
@@ -27,19 +31,23 @@ int main(int argc, char *argv[])
 // }
 
 
-// int main(int argc, char *argv[])
-// {
-  int data_processed;
-  char buffer[BUFSIZ + 1];
-  int file_descriptor;
-  memset(buffer, '\0', sizeof(buffer));   
+  //int data_processed;
+  //char buffer[BUFSIZ + 1];
+  //int file_descriptor;
+  //assert(argc >= 3);
+  //memset(buffer, '\0', sizeof(buffer));
+  //file_descriptor = atoi(argv[1]);
+  //assert(file_descriptor == 5);
   sscanf(argv[1], "%d", &file_descriptor);
-  // sscanf(argv[2], "%d", &child_to_parent[READ]);
-  // sscanf(argv[3], "%d", &child_to_parent[WRITE]);
-  while(strcmp(buffer, "quit")) {
-    data_processed = read(file_descriptor, buffer, BUFSIZ);
-    printf("%d - read %d bytes: %s\n", getpid(), data_processed, buffer);
-    memset(buffer, '\0', sizeof(buffer));   
-  }
+  sscanf(argv[2], "%d", &child_to_parent);
+  printf("file_descriptor = %d\n", file_descriptor);
+  printf("child_to_parent = %d\n", child_to_parent); 
+// sscanf(argv[3], "%d", &child_to_parent[WRITE]);
+//  while(strcmp(buffer, "quit")) {
+//    data_processed = read(file_descriptor, buffer, BUFSIZ);
+//    printf("%d - read %d bytes: %s\n", getpid(), data_processed, buffer);
+//    memset(buffer, '\0', sizeof(buffer));
+//  }
+  printf("Child process exits\n");
   exit(EXIT_SUCCESS);
 }
--- main_parent.cpp
+++ main_parent.cpp
@@ -2,13 +2,17 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<assert.h>
+#include <string>
 
 int main()
 {
   int data_processed;
+  //any data written to fd[1] 
+  //can be read back from fd[0]
   int parent_to_child[2];
   int child_to_parent[2];
-  pipe(child_to_parent);
+  assert(pipe(child_to_parent) == 0);
 
   char some_data[100];
   char buffer[BUFSIZ + 1];
@@ -21,14 +25,26 @@ int main()
       exit(EXIT_FAILURE);
     }
     if (fork_result == 0) {
-      sprintf(buffer, "%d %d %d", parent_to_child[0], child_to_parent[0], child_to_parent[1]);
-  
-      (void)execl("app2", "app2", buffer, (char *)0);
+      //sprintf(buffer, "%d %d", parent_to_child[0], child_to_parent[1]);
+      //printf("Starting child with parameters: %s\n", buffer);
+      //sleep(1);
+       //exec child process
+      //const char filename[] = "app2";
+      //char const* newargv[] = { std::to_string(parent_to_child[0]).c_str(), 
+	//		  std::to_string(child_to_parent[1]).c_str() };
+      //char const* newenviron[] = { NULL };
+      //execve(filename, newargv, newenviron);
+      execl("./app2", "app2", std::to_string(parent_to_child[0]).c_str(),
+                              std::to_string(child_to_parent[1]).c_str(), (char *)0);
+      //execl("./app2", "app2", buffer, (char *)0);
+      //execl("./app2", "app2", (char *)0);
+      printf("Child process failed\n");	
       exit(EXIT_FAILURE);
     }
     else {
       do {
         scanf("%s", some_data);
+        strcat(some_data,"\n");
         data_processed = write(parent_to_child[1], some_data, strlen(some_data));      
         printf("%d - wrote %d bytes\n", getpid(), data_processed);
       } while(strcmp(some_data, "quit"));
